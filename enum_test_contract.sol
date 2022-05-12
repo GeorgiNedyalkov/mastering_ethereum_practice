058cb554ef63d37c2aea5978ddcb4095eb84b4ee
Flu3nt <gnedyalkov94@gmail.com>
2022-05-12 15:31:51 +0300

    Created a basic contract to get a feel for enumerables

--- enum_test_contract.sol
+++ enum_test_contract.sol
@@ -2,5 +2,23 @@ pragma solidity ^0.8.0;
 
 contract EnumTest {
     enum ActionChoices { GoLeft, GoRight, GoRight, GoStraight, SitStill }
+    ActionChoices choice;
+    ActionChoices constant defaultChoice = ActionChoices.GoStraight;
+
+    function setGoStraigh() public {
+        choice = ActionChoices.GoStraight;
+    }
+
+    function getChoice() public pure returns (uint) {
+        return uint(defaultChoice);
+    }
+
+    function getLargestValue() public pure returns (ActionChoices) {
+        return type(ActionChoices).max;
+    }
+
+    function getSmallesttValue() public pure returns (ActionChoices) {
+        return type(ActionChoices).min;
+    }
+
     
-}
