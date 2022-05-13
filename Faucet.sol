99f68f593b90139853830bd275d81526e41ced7a
Flu3nt <gnedyalkov94@gmail.com>
2022-05-13 12:30:45 +0300

    Created events for withdraws and deposits

--- Faucet.sol
+++ Faucet.sol
@@ -6,20 +6,26 @@ contract Faucet is Mortal{
 
     constructor() {
         owner = msg.sender;
-    }
-    
-    receive() external payable;
+    } 
+
+    event Withdrawa(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);
 
     modifier onlyOnwer {
         require(msg.sender == owner, "Only the contract owner can call this function");
         _;
     }
 
+    receive() external payable {
+        emit(msg.sender, msg.value);
+    };
+
     function withdraw(uint withdraw_amount) public {
         require(withdraw_amount < 0.1 ethers);
         require(this.balance >= withdraw_amount,
         "Insufficient balance in faucet for withdrawal request");
         msg.sender.transfer(withdraw_amount);
+        emit(msg.sender, withdraw_amount);
     }
 
     function destroy() public onlyOwner {
