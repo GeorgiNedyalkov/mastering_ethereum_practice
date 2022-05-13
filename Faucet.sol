a2446eaf30af1e00b3190ae02bd751436d17b7f9
Flu3nt <gnedyalkov94@gmail.com>
2022-05-13 12:38:24 +0300

    Added other contracts into Fauce.sol and refactored code by removing duplicates

--- Faucet.sol
+++ Faucet.sol
@@ -1,34 +1,41 @@
 pragma solidity ^0.8.0;
 
-contract Faucet is Mortal{
-    
+contract Owned {
     address owner;
 
     constructor() {
         owner = msg.sender;
-    } 
-
-    event Withdrawa(address indexed to, uint amount);
-    event Deposit(address indexed from, uint amount);
+    }
 
     modifier onlyOnwer {
         require(msg.sender == owner, "Only the contract owner can call this function");
         _;
     }
+}
 
-    receive() external payable {
-        emit(msg.sender, msg.value);
-    };
 
-    function withdraw(uint withdraw_amount) public {
-        require(withdraw_amount < 0.1 ethers);
-        require(this.balance >= withdraw_amount,
-        "Insufficient balance in faucet for withdrawal request");
-        msg.sender.transfer(withdraw_amount);
-        emit(msg.sender, withdraw_amount);
-    }
-
+contract Mortal is Owned {
     function destroy() public onlyOwner {
         selfdestruct(owner);
-    }    
+    }
+}
+
+
+contract Faucet is Mortal{   
+    event Withdrawa(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);    
+
+    function withdraw(uint withdraw_amount) public {
+            require(withdraw_amount < 0.1 ethers);
+            require(this.balance >= withdraw_amount,
+                "Insufficient balance in faucet for withdrawal request");
+
+        msg.sender.transfer(withdraw_amount);
+            emit(msg.sender, withdraw_amount);
+    }
+
+    function () external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+     
 }
--- Mortal.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract Mortal is Owned {
-    function destroy() public onlyOwner {
-        selfdestruct(owner);
-    }
-}
